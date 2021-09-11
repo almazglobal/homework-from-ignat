ac6a4a5c130d9603a615577184c806615087bb1c
Alex <sasha_samara@bk.ru>
2021-09-11 10:51:02 +0400

    Lesson 4. To do style for checkbox and input

--- src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -20,7 +20,12 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // сделайте так чтоб работал onChange и onChangeChecked
+        debugger
+        onChange
+        && onChange(e)
+
+        onChangeChecked
+        && onChangeChecked(e.currentTarget.checked)
     }
 
     const finalInputClassName = `${s.checkbox} ${className ? className : ''}`
