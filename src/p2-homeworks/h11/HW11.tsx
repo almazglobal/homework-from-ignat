a4366b0de29be7a7e99a6bd9b8d0e46aa1bb98dc
Alex <almazlocal@gmail.com>
2021-11-10 16:57:05 +0400

    Done lesson #11.

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import styles from './HW11.module.css'
 
 function HW11() {
     const [value1, setValue1] = useState(0)
@@ -8,30 +9,38 @@ function HW11() {
 
     return (
         <div>
-            <hr/>
+            <hr />
             homeworks 11
 
             {/*should work (должно работать)*/}
-            <div>
+            <div className={styles.wrapper}>
                 <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+
+                    <SuperRange value1={value1}
+                        onChangeRange={(value1) => setValue1(value1)}
+                    />
+
             </div>
 
-            <div>
+            <div className={styles.wrapper}>
                 <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
+
+                    <SuperDoubleRange value1={value1}
+                                      value2={value2}
+                                      onInput1={setValue1}
+                                      onInput2={setValue2}
+                                      disable={false}
+                        // сделать так чтоб value1 и value2 изменялось
+                    />
+
                 <span>{value2}</span>
             </div>
 
-            <hr/>
+            <hr />
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperRange/>*/}
             {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
+            <hr />
         </div>
     )
 }
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,84 @@
-import React from 'react'
+import React, {ChangeEvent, DetailedHTMLProps, FormEvent, InputHTMLAttributes, useState} from 'react'
+import styles from './SuperDoubleRange.module.css'
 
-type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+type SuperDoubleRangePropsType = DefaultInputPropsType & {
+    // onChangeRange?: (value: number) => void
+    // value?: [number, number]
+    value1: number
+    value2: number
+    onInput1: (value1: number) => void
+    onInput2: (value2: number) => void
+    disable: boolean
     // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
+        // onChangeRange, value,
         // min, max, step, disable, ...
+        value1, value2,
+        onInput1, onInput2,
+        disable,
+        ...restProps
     }
 ) => {
     // сделать самому, можно подключать библиотеки
+    let minGap = 0;
+    let minActiveRange = 5
+    let sliderMaxValue = 100;
+    let sliderTrackStyleBackground = '';
+
+    const [activeRange, setActiveRange] = useState(1)
+
+    function slideOne(e: FormEvent<HTMLInputElement>) {
+        onInput1(parseInt(e.currentTarget.value))
+        if (value2 - parseInt(e.currentTarget.value) <= minGap) {
+            onInput1(value2 - minGap);
+        }
+        if (value2 - parseInt(e.currentTarget.value) <= minGap + minActiveRange) {
+            setActiveRange(1)
+        }
+    }
+
+    function slideTwo(e: FormEvent<HTMLInputElement>) {
+        onInput2(parseInt(e.currentTarget.value))
+        if (parseInt(e.currentTarget.value) - value1 <= minGap) {
+            onInput2(value1 + minGap);
+        }
+        if (parseInt(e.currentTarget.value) - value1 <= minGap + minActiveRange) {
+            setActiveRange(2)
+        }
+    }
+
+    function fillColor() {
+        const percent1 = (value1 / sliderMaxValue) * 100;
+        const percent2 = (value2 / sliderMaxValue) * 100;
+        sliderTrackStyleBackground = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`
+    }
+
+    fillColor();
 
     return (
-        <>
-            DoubleRange
-        </>
+        <div className={`${styles.container} ${disable ? styles.disabled : ''}`}>
+            <div className={styles.sliderTrack}
+                 style={{background: sliderTrackStyleBackground}}></div>
+            <input type="range"
+                   value={value1}
+                   style={{zIndex: activeRange == 1 ? 1 : 0}}
+                   onInput={slideOne}
+                   disabled={disable}
+                   {...restProps}
+            />
+            <input type="range"
+                   value={value2}
+                   style={{zIndex: activeRange == 2 ? 1 : 0}}
+                   onInput={slideTwo}
+                   disabled={disable}
+                   {...restProps}
+            />
+        </div>
     )
 }
 
